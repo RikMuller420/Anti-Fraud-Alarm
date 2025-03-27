e7aac50436165177ca5023912792e20f46b7b693
Muller Rik <rikmuller420@gmail.com>
2025-03-27 13:14:52 +0300

    Создан класс сигнализации

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField, Range(0f, 1f)] private float _maxVolume = 1f;
+    [SerializeField, Min(0.1f)] private float _volumeChangeStep = 1f;
+
+    private Coroutine _volumeChangeCoroutine;
+
+    public void Activate()
+    {
+        _audioSource.Play();
+        StopVolumeChangeCourutine();
+        _volumeChangeCoroutine = StartCoroutine(ChangeSoundVolume(_maxVolume));
+    }
+
+    public void Deactivate()
+    {
+        StopVolumeChangeCourutine();
+        _volumeChangeCoroutine = StartCoroutine(ChangeSoundVolume(0f));
+    }
+
+    private void StopVolumeChangeCourutine()
+    {
+        if (_volumeChangeCoroutine != null)
+        {
+            StopCoroutine(_volumeChangeCoroutine);
+        }
+    }
+
+    private IEnumerator ChangeSoundVolume(float tagetVolume)
+    {
+        while (_audioSource.volume != tagetVolume)
+        {
+            float volumeChangeStep = _volumeChangeStep * Time.deltaTime;
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, tagetVolume, volumeChangeStep);
+
+            yield return null;
+        }
+
+        if (tagetVolume == 0)
+        {
+            _audioSource.Stop();
+        }
+    }
+}
